0289c297cefcca849920338fba838d1a8966fc53
Jon Seddon <jon.seddon@metoffice.gov.uk>
2019-09-24 13:34:55 +0100

    Handle sym links

--- scripts/update_dreqs/update_dreqs_0220.py
+++ scripts/update_dreqs/update_dreqs_0220.py
@@ -12,7 +12,7 @@ import sys
 
 import django
 django.setup()
-from pdata_app.models import DataRequest
+from pdata_app.models import DataRequest, Settings
 from pdata_app.utils.common import construct_drs_path, delete_drs_dir, get_gws
 
 __version__ = '0.1.0b1'
@@ -23,6 +23,10 @@ DEFAULT_LOG_FORMAT = '%(levelname)s: %(message)s'
 logger = logging.getLogger(__name__)
 
 NEW_VERSION = 'v20190923'
+
+# The top-level directory to write output data to
+BASE_OUTPUT_DIR = Settings.get_solo().base_output_dir
+
 
 def parse_args():
     """
@@ -46,13 +50,16 @@ def main(args):
         climate_model__short_name__in=['MPI-ESM1-2-HR'], #'MPI-ESM1-2-XR'],
         experiment__short_name='highresSST-present',
         variable_request__table_name='Amon',
-        variable_request__cmor_name='tas',
+        variable_request__cmor_name='tasmax',
     ).exclude(
         climate_model__short_name='MPI-ESM1-2-XR',
         variable_request__cmor_name__in=['hus7h', 'ta7h', 'ua7h']
     )
 
+    logger.debug(f'Found {dreqs.count()} data requests')
+
     for dreq in dreqs:
+        logger.debug(f'Processing {dreq}')
         old_directories = []
         for df in dreq.datafile_set.order_by('name'):
             if not df.online:
@@ -61,6 +68,10 @@ def main(args):
             if df.version == NEW_VERSION:
                 logger.warning(f'Already at {NEW_VERSION} {df.name}')
                 continue
+            # save the sym link directory before we make any changes
+            old_sym_link_dir = os.path.join(BASE_OUTPUT_DIR,
+                                            construct_drs_path(df))
+            # now get back to updating the version
             df.version = NEW_VERSION
             gws = get_gws(df.directory)
             new_dir = os.path.join(gws, construct_drs_path(df))
@@ -73,12 +84,25 @@ def main(args):
             df.save()
             if old_directory not in old_directories:
                 old_directories.append(old_directory)
+            # Update any sym links too
+            sym_link_path = os.path.join(old_sym_link_dir, df.name)
+            if os.path.lexists(sym_link_path):
+                if os.path.islink(sym_link_path):
+                    os.remove(sym_link_path)
+                    if old_sym_link_dir not in old_directories:
+                        old_directories.append(old_sym_link_dir)
+            sym_link_dir = os.path.join(BASE_OUTPUT_DIR,
+                                        construct_drs_path(df))
+            if not os.path.exists(sym_link_dir):
+                os.makedirs(sym_link_dir)
+            sym_link_path = os.path.join(sym_link_dir, df.name)
+            os.symlink(os.path.join(df.directory, df.name), sym_link_path)
 
-    for directory in old_directories:
-        if not os.listdir(directory):
-            delete_drs_dir(directory)
-        else:
-            logger.error(f'Not empty {directory}')
+        for directory in old_directories:
+            if not os.listdir(directory):
+                delete_drs_dir(directory)
+            else:
+                logger.error(f'Not empty {directory}')
 
 
 if __name__ == "__main__":
